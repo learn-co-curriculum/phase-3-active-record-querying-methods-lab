0978824fb4e0106d1ad50f4cd1a894ed50f21bc4
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2015-11-18 17:44:08 -0500

    tests for all Show querying methods, need to write readme

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,26 +1,30 @@
 class Show < ActiveRecord::Base
 
-  def self.most_popular
+  def self.highest_rating
     self.maximum(:rating)
   end
 
-  def self.least_popular
+  def self.most_popular_show
+    self.where("rating = ?", self.highest_rating).first
+  end
+
+  def self.lowest_rating
     self.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    self.where("rating = ?", self.lowest_rating).first
   end
 
   def self.ratings_sum
     self.sum(:rating)
   end
   
-  def shows_by_rating
-    self.group(:rating).count
+  def self.popular_shows
+    self.where("rating >= ?", 5)
   end
 
-  def popular_shows
-    self.where("rating >= ?", 60)
-  end
-
-  def shows_by_alphabetical_order
+  def self.shows_by_alphabetical_order
     self.order(:name)
   end
 end
--- spec/show_spec.rb
+++ spec/show_spec.rb
@@ -2,23 +2,73 @@ require_relative 'spec_helper'
 
 describe Show do
 
-  let(:rick_and_morty) { Show.create(:name => "Rick and Morty", :day => "Thursday", :network => "Adult Swim", :rating => 10) }
-  let(:law_and_order) {Show.create(:name => "Law & Order", :day => "Monday", :network => "NBC", :rating => "7")}
-  let(:the_cleveland_show) {Show.create(:name => "The Cleveland Show", :day => "Monday", :network => "Fox", :rating => "2")}
-  let(:fear_the_walking_dead) {Show.create(:name => "Fear the Walking Dead", :day => "Sunday", :network => "AMC", :rating => "3")}
-  let(:law_and_order) {Show.create(:name => "Law & Order", :day => "Monday", :network => "NBC", :rating => "7")}
+  after(:each) do 
+    Show.destroy_all
+  end
 
-  it "has data attributes" do
+  before(:each) do
+    @rick_and_morty = Show.create(:name => "Rick and Morty", :day => "Thursday", :network => "Adult Swim", :rating => 10)
+    @law_and_order = Show.create(:name => "Law & Order", :day => "Monday", :network => "NBC", :rating => "7")
+    @the_cleveland_show = Show.create(:name => "The Cleveland Show", :day => "Monday", :network => "Fox", :rating => "2")
+    @fear_the_walking_dead = Show.create(:name => "Fear the Walking Dead", :day => "Sunday", :network => "AMC", :rating => "3")
+  end
+
+  describe "attributes" do 
+    it "has data attributes" do
     #TODO: You'll need to create a new migration to add
     #the necessary columns to the shows table
-    expect(community.name).to eq("Rick and Morty")
-    expect(community.day).to eq("Thursday")
-    expect(community.network).to eq("Adult Swim")
-    expect(community.rating).to eq(10)
+      expect(@rick_and_morty.name).to eq("Rick and Morty")
+      expect(@rick_and_morty.day).to eq("Thursday")
+      expect(@rick_and_morty.network).to eq("Adult Swim")
+      expect(@rick_and_morty.rating).to eq(10)
+    end
   end
 
-  describe "#most_popular" do 
-
+  describe "#highest_rating" do 
+    it "returns the TV show with the highest rating" do 
+      expect(Show.highest_rating).to eq(10)
+    end
   end
+
+  describe "#most_popular_show" do
+    it "returns the tv show with the highest rating" do 
+      expect(Show.most_popular_show).to eq(@rick_and_morty)
+    end 
+  end
+
+  describe "#lowest_rating" do 
+    it "returns the TV show with the lowest rating" do 
+      expect(Show.lowest_rating).to eq(2)
+    end
+  end
+
+  describe "#least_popular_show" do
+    it "returns the tv show with the lowest rating" do 
+      expect(Show.least_popular_show).to eq(@the_cleveland_show)
+    end 
+  end
+
+  describe "#ratings_sum" do 
+    it "returns the sum of all the ratings of all the tv shows" do 
+      expect(Show.ratings_sum).to eq(22)
+    end
+  end
+
+  describe "#popular_shows" do 
+    it "returns an array of all of the shows with a rating above 5" do 
+      expect(Show.popular_shows).to eq([@rick_and_morty, @law_and_order])
+    end
+  end
+
+  describe "#shows_by_alphabetical_order" do 
+    it "returns an array of all of the shows, listed in alphabetical order" do 
+      expect(Show.shows_by_alphabetical_order).to eq([@fear_the_walking_dead, @law_and_order, @rick_and_morty, @the_cleveland_show])
+    end
+  end
+
+
+
+
+
 
 end
