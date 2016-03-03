1d2d7d634d89ac65fa3baf2aea3e74c983c94385
vicfriedman <victoria.m.friedman@gmail.com>
2016-03-03 09:43:09 -0500

    fix environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,4 @@
+
 require "bundler/setup"
 require 'yaml'
 require 'active_record'
@@ -15,4 +16,3 @@ DB = ActiveRecord::Base.establish_connection({
 if ENV["ACTIVE_RECORD_ENV"] == "test"
   ActiveRecord::Migration.verbose = false
 end
-Status API Training Shop Blog About Pricing
