360e760c549071accf81eece03202d7827844adb
Charles Pletcher <cpletcher2@gmail.com>
2016-04-22 16:27:24 -0400

    These are class, not instance, methods
    Fixes #13

--- spec/show_spec.rb
+++ spec/show_spec.rb
@@ -2,7 +2,7 @@ require_relative 'spec_helper'
 
 describe Show do
 
-  after(:each) do 
+  after(:each) do
     Show.destroy_all
   end
 
@@ -13,7 +13,7 @@ describe Show do
     @fear_the_walking_dead = Show.create(:name => "Fear the Walking Dead", :day => "Sunday", :network => "AMC", :rating => 3)
   end
 
-  describe "attributes" do 
+  describe "attributes" do
     it "has data attributes given to it from a migration file" do
     #TODO: You'll need to create a new migration to add
     #the necessary columns to the shows table
@@ -23,62 +23,56 @@ describe Show do
       expect(@rick_and_morty.rating).to eq(10)
     end
 
-    it "has a season" do 
+    it "has a season" do
       game_of_thrones = Show.create(:name => "Game of Thrones", :day => "Sunday", :season => "Spring", :network => "HBO", :rating => 10)
       expect(game_of_thrones.season).to eq("Spring")
     end
 
-    it "has a season column and attribute added from another migration file" do 
+    it "has a season column and attribute added from another migration file" do
       filepath = File.expand_path(File.dirname(File.dirname(__FILE__))) + "/db/migrate"
       expect(Dir.entries(filepath)).to include("002_add_season_to_shows.rb")
     end
   end
 
-  describe "#highest_rating" do 
-    it "returns the TV show with the highest rating" do 
+  describe "::highest_rating" do
+    it "returns the TV show with the highest rating" do
       expect(Show.highest_rating).to eq(10)
     end
   end
 
-  describe "#most_popular_show" do
-    it "returns the tv show with the highest rating" do 
+  describe "::most_popular_show" do
+    it "returns the tv show with the highest rating" do
       expect(Show.most_popular_show).to eq(@rick_and_morty)
-    end 
+    end
   end
 
-  describe "#lowest_rating" do 
-    it "returns the TV show with the lowest rating" do 
+  describe "::lowest_rating" do
+    it "returns the TV show with the lowest rating" do
       expect(Show.lowest_rating).to eq(2)
     end
   end
 
-  describe "#least_popular_show" do
-    it "returns the tv show with the lowest rating" do 
+  describe "::least_popular_show" do
+    it "returns the tv show with the lowest rating" do
       expect(Show.least_popular_show).to eq(@the_cleveland_show)
-    end 
+    end
   end
 
-  describe "#ratings_sum" do 
-    it "returns the sum of all the ratings of all the tv shows" do 
+  describe "::ratings_sum" do
+    it "returns the sum of all the ratings of all the tv shows" do
       expect(Show.ratings_sum).to eq(22)
     end
   end
 
-  describe "#popular_shows" do 
-    it "returns an array of all of the shows with a rating above 5" do 
+  describe "::popular_shows" do
+    it "returns an array of all of the shows with a rating above 5" do
       expect(Show.popular_shows).to eq([@rick_and_morty, @law_and_order])
     end
   end
 
-  describe "#shows_by_alphabetical_order" do 
-    it "returns an array of all of the shows, listed in alphabetical order" do 
+  describe "::shows_by_alphabetical_order" do
+    it "returns an array of all of the shows, listed in alphabetical order" do
       expect(Show.shows_by_alphabetical_order).to eq([@fear_the_walking_dead, @law_and_order, @rick_and_morty, @the_cleveland_show])
     end
   end
-
-
-
-
-
-
 end
