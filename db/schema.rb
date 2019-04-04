0fd052ae71fe82f5d8539a5ff984630194212236
maxwellbenton <maxwellbenton@gmail.com>
2019-04-04 09:55:19 -0400

    solution schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2) do
+
+  create_table "shows", force: :cascade do |t|
+    t.string "name"
+    t.string "day"
+    t.string "network"
+    t.integer "rating"
+    t.string "season"
+  end
 
 end
