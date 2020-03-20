2bd9fc801ee1e66082b86b97775d735b6a1024d1
Matthew J. Campbell <matjocampbell@gmail.com>
2020-03-20 10:39:45 -0700

    Changed '::highest_rating' to '.highest_rating'
    Both are proper ways of denoting a class method, but we tend to refer to class methods using the  'Show.highest_rating' notation rather than 'Show::highest_rating'.

--- spec/show_spec.rb
+++ spec/show_spec.rb
@@ -34,43 +34,43 @@ describe Show do
     end
   end
 
-  describe "::highest_rating" do
+  describe ".highest_rating" do
     it "returns the rating of the TV show with the highest rating" do
       expect(Show.highest_rating).to eq(10)
     end
   end
 
-  describe "::most_popular_show" do
+  describe ".most_popular_show" do
     it "returns the name of the TV show with the highest rating" do
       expect(Show.most_popular_show).to eq(@rick_and_morty)
     end
   end
 
-  describe "::lowest_rating" do
+  describe ".lowest_rating" do
     it "returns the rating of the TV show with the lowest rating" do
       expect(Show.lowest_rating).to eq(2)
     end
   end
 
-  describe "::least_popular_show" do
+  describe ".least_popular_show" do
     it "returns the name of the TV show with the lowest rating" do
       expect(Show.least_popular_show).to eq(@the_cleveland_show)
     end
   end
 
-  describe "::ratings_sum" do
+  describe ".ratings_sum" do
     it "returns the sum of all the ratings of all the tv shows" do
       expect(Show.ratings_sum).to eq(22)
     end
   end
 
-  describe "::popular_shows" do
+  describe ".popular_shows" do
     it "returns an array of all of the shows with a rating above 5" do
       expect(Show.popular_shows).to eq([@rick_and_morty, @law_and_order])
     end
   end
 
-  describe "::shows_by_alphabetical_order" do
+  describe ".shows_by_alphabetical_order" do
     it "returns an array of all of the shows, listed in alphabetical order" do
       expect(Show.shows_by_alphabetical_order).to eq([@fear_the_walking_dead, @law_and_order, @rick_and_morty, @the_cleveland_show])
     end
