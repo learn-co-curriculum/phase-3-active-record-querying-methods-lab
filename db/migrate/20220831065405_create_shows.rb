72e70151ed831f07c3916e8bccad91c64cc754fd
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-08-31 10:11:42 +0300

    commit tree 2ff525246858cf00e5e0b2d98be3540b520f1852

---
+++ db/migrate/20220831065405_create_shows.rb
@@ -0,0 +1,10 @@
+class CreateShows < ActiveRecord::Migration[6.1]
+  def change
+    create_table :shows do |t|
+      t.string :name
+      t.string :network
+      t.string :day
+      t.integer :rating
+    end
+  end
+end
